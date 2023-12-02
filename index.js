a58691ced053f7fed1190962c7c41ab828fa9d9b
Kartik Shukla <144674234+kartikshukla17@users.noreply.github.com>
2023-12-03 02:13:23 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+<script language ="javascript" type="text/javascript">
+var a , b , c
+a=prompt("write the radius of circle")
+b=2*22/7*a
+c=22/7*a*a
+document.write("the circumference of circle "+b+"<br>")
+document.write("the area of circle " + c+"<br>")
+</script>
