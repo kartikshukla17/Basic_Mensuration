e9dcc8c648a8f3ed5ced1ebd46306376ba7c8946
Kartik Shukla <144674234+kartikshukla17@users.noreply.github.com>
2023-12-03 04:02:49 +0530

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,4 +1,4 @@
-<script language="javascript">
+
 var a,b,c,d,e
 a=prompt("write the side of traiangle if you want to know it perimeter")
 b=prompt("write the base of triangle")
@@ -7,4 +7,3 @@ d=3*a
 e=1/2*b*c
 document.write("the perimeter of triangle is "+ d+"<br>")
 document.write("the area of triangle is " + e+"<br>")
-</script>
