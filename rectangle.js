8fa9a463ceaf672b1ddcf8b2b40c1fdca66235f0
Kartik Shukla <144674234+kartikshukla17@users.noreply.github.com>
2023-12-03 02:18:42 +0530

    Create rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,11 @@
+<script language="javascript" type="text/javascript">
+var a, b , c , d
+a=prompt("write the length of rectangle")
+b=prompt("write the breadth of rectangle")
+parseInt(a);
+parseInt(b);
+c=a*b
+d=a+b*2
+document.write("the area of your rectangle " + c+"<br>")
+document.write("the perimeter of your rectangle " +d+"<br>")
+</script>
