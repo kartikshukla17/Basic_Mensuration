404f202f51cc02191f0c74d3fb8130a1e7bbbe0a
Kartik Shukla <144674234+kartikshukla17@users.noreply.github.com>
2023-12-03 04:02:29 +0530

    Update square.js

--- square.js
+++ square.js
@@ -1,8 +1,8 @@
-<script language="javascript">
+
 var a,b,c
 a=prompt("write the side of square")
 b=4*a
 c=a*a
 document.write("the perimeter of square is " + b+"<br>")
 document.write("the area of square is " + c+"<br>")
-</script>
+
