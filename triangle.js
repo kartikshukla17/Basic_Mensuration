0f83700032707bd788e2769dc897f214b15fb516
Kartik Shukla <144674234+kartikshukla17@users.noreply.github.com>
2023-12-03 02:15:50 +0530

    Create triangle.js

---
+++ triangle.js
@@ -0,0 +1,10 @@
+<script language="javascript">
+var a,b,c,d,e
+a=prompt("write the side of traiangle if you want to know it perimeter")
+b=prompt("write the base of triangle")
+c=prompt("write the hieght of triangle")
+d=3*a
+e=1/2*b*c
+document.write("the perimeter of triangle is "+ d+"<br>")
+document.write("the area of triangle is " + e+"<br>")
+</script>
